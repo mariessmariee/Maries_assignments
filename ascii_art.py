d9dd9d71a2aeb6c3d000c5191c4fe7f2df8461f1
mariessmariee <marie.sinkovic@gmail.com>
2025-04-14 01:14:00 +0200

    ASCII-Art-Datei erstellt und Python-Code angepasst

--- ascii_art.py
+++ ascii_art.py
@@ -1,12 +1,20 @@
-print("    /\ ")
-print("   /  \ ")
-print("  /----\ ")
-print("  |    |")
-print("  |    |")
-print("  |    |")
-print("  /----\ ")
-print("    ||  ")
-print("    ||  ")
-print("   /||\ ")
-print("    **  ")
-print("   **** ")
+ascii_art = """
+    /\\ 
+   /  \\ 
+  /----\\ 
+  |    |
+  |    |
+  |    |
+  /----\\ 
+    ||  
+    ||  
+   /||\\ 
+    **  
+   **** 
+"""
+
+
+with open("ascii_art.txt", "w") as file:
+    file.write(ascii_art)
+
+print(ascii_art)
